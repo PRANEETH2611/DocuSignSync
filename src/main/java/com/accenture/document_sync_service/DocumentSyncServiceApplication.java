1562b5250b9be12a129a275f07a3d65ca6daf811
Reddy <katukuri.p.reddy@accenture.com>
2026-06-29 17:00:08 +0530

    Phase 1.2: Configure Spring Boot project and dependencies

---
+++ src/main/java/com/accenture/document_sync_service/DocumentSyncServiceApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.document_sync_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentSyncServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentSyncServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/document_sync_service/DocumentSyncServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.document_sync_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentSyncServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
